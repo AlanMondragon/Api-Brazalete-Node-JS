2681391baadb3f07b2c3100f6d973c0ef199d97f
AlanMondragon <20233tn075@utez.edu.mx>
2025-03-26 12:46:09 -0600

    Actualizar cada que recibe el tiempo

--- src/routes/BraceletRouters.js
+++ src/routes/BraceletRouters.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const brazaletController = require('../controllers/braceletController');
 const brazaletController = require('../controllers/BraceletController');
 
 // Crear una pulsera
